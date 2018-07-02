4ad934e2bb9ee12a218dfcb797923d5abea9604e
fessmm <furkaaan28@live.de>
2018-07-02 11:10:39 +0200

    RRify
    Change-Id: I50df0337107d26e0f8020d03d846e8d1148ecf7f

--- rr.mk
+++ rr.mk
@@ -16,9 +16,9 @@
 #
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
 PRODUCT_DEVICE := klte
-PRODUCT_NAME := lineage_klte
+PRODUCT_NAME := rr_klte
